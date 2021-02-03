3e2a38fa4be80130e0392d42c05516e645cd535e
RenatoDeAquino <repitriano12@gmail.com>
2021-02-03 18:17:00 -0300

    alteração na chance das armas

--- weapon.py
+++ weapon.py
@@ -3,7 +3,7 @@ weapon_list = ['knife', 'sword', 'long sword', 'spear']
 
 def gera_weapon():
     weapon_chance = random.randint(1,10)
-    if weapon_chance == 1 or weapon_chance == 2:
+    if weapon_chance == 1 or weapon_chance == 3:
         weapon_choice = random.randint(0,len(weapon_list))
         return weapon_list[weapon_choice]
     return None
